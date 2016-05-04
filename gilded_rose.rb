e3851ed2f3a4174b466d71289fac44f5d5a0b23a
michael <fernandez.michaeljohn@gmail.com>
2016-05-04 16:13:21 +0800

    modify gilded_rose

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -4,54 +4,64 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      update_item(item)    
     end
   end
+
+  private
+
+  def update_item(item)
+    
+    item.sell_in -= 1
+
+    case
+
+    when item.name == "Aged Brie"
+      return unless item.quality < 50
+        item.quality += 1
+
+    when item.name == "Backstage passes to a TAFKAL80ETC concert"
+      return unless item.quality < 50
+
+      if item.sell_in < 0
+        item.quality = 0    
+
+      elsif item.sell_in < 6  
+        item.quality += 3
+
+      elsif item.sell_in < 11
+        item.quality += 2
+
+      else
+        item.quality += 1
+      end 
+
+    # when /conjured/.match('conjured item')
+    #   if item.sell_in > 0
+    #     item.quality -= 2
+    #   else
+    #     item.quality -= 4
+    #   end
+        
+    else
+      if item.sell_in > 0
+        item.quality -= 1
+      else
+        item.quality -= 2    
+      end
+    end
+    
+    if item.quality < 0 
+      item.quality = 0
+    elsif item.quality > 50
+      item.quality = 50
+    end
+
+  end  
 end
+
 
 class Item
   attr_accessor :name, :sell_in, :quality
